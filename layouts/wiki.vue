b48260f7bdab22ad1f216c8b5bd1b8041ce0a33e
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-06-14 23:45:09 +0200

    also fixed many bugs with scrolling

--- layouts/wiki.vue
+++ layouts/wiki.vue
@@ -1,5 +1,5 @@
 <template>
-  <navbar />
+  <navbar id="navbar" />
   <div class="bg-wiki"></div>
   <div class="wiki-hero">
     <sidebar />
@@ -20,9 +20,30 @@
   background: #0d1117 !important;
   width: 100%;
   height: 100%;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   z-index: -1;
+}
+
+.wiki-hero {
+  top: 104px;
+}
+
+@media (max-width: 600px) {
+  .wiki-hero {
+    top: 65px;
+  }
+}
+
+.content {
+  margin-bottom: 30px;
+}
+
+#navbar {
+  position: fixed;
+  z-index: 100;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
