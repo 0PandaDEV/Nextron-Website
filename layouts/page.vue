8ceaf4384c4b17d210af3524fd834168d41e1905
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-06-14 15:09:36 +0200

    added page layout

---
+++ layouts/page.vue
@@ -0,0 +1,34 @@
+<template>
+  <navbar />
+  <div class="bg-page"></div>
+  <div class="center">
+    <div class="page-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+.bg-page {
+  background: #0d1117 !important;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.page-content {
+  display: flex;
+  max-width: 1200px;
+  padding: 32px;
+}
+</style>
