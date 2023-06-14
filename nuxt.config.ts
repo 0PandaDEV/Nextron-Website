025f92635e1d5418d72e796d2f2bbe90e4000461
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-06-14 19:04:57 +0200

    tried hotjar analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/robots',
-    'nuxt-simple-sitemap',
-    '@nuxt/image'
-  ],
-
+  modules: ["@nuxtjs/robots", "nuxt-simple-sitemap", "@nuxt/image"],
   devtools: true,
-
   app: {
     head: {
       script: [
-        { src: 'https://www.googletagmanager.com/gtag/js?id=G-ZP030NNLMF', async: true }
-      ]
-    }
+        {
+          innerHTML: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3534428,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `,
+        },
+      ],
+    },
   },
-
-  plugins: ['~/plugins/google-analytics.js']
-})
+});
