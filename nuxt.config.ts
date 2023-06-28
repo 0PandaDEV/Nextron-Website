a45ab917c055f162694f808226ce7ce68ce1fac7
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-06-28 19:36:58 +0200

    updated packages and added smartlook analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,21 @@ export default defineNuxtConfig({
   },
 
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
+  app: {
+    head: {
+      script: [
+        {
+          type: "text/javascript",
+          src: "https://web-sdk.smartlook.com/recorder.js",
+          charset: "utf-8",
+          async: true,
+        },
+        {
+          type: "text/javascript",
+          innerHTML:
+            "smartlook('init', 'cc797f15bf9bdb3b144ff31b8874f45e9d97c83e', { region: 'eu' });",
+        },
+      ],
+    },
+  },
 });
