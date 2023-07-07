099b986ec68507b20d489a700e87b60aae617f34
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-07-07 11:20:26 +0200

    umami analytics less goo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,20 +6,13 @@ export default defineNuxtConfig({
     enabled: true,
   },
 
-  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   app: {
     head: {
       script: [
         {
-          type: "text/javascript",
-          src: "https://web-sdk.smartlook.com/recorder.js",
-          charset: "utf-8",
+          src: "https://umami-pandadev.vercel.app/script.js",
           async: true,
-        },
-        {
-          type: "text/javascript",
-          innerHTML:
-            "smartlook('init', 'cc797f15bf9bdb3b144ff31b8874f45e9d97c83e', { region: 'eu' });",
+          "data-website-id": "72ea9501-3b4c-4202-a406-857bf85833b2",
         },
       ],
     },
