b2f9d402cf021ae09e31fee1cc1e9e6db317f3d7
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2023-11-07 17:28:10 +0100

    added plausible analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,9 @@ export default defineNuxtConfig({
     head: {
       script: [
         {
-          src: "https://umami-pandadev.vercel.app/script.js",
-          async: true,
-          "data-website-id": "72ea9501-3b4c-4202-a406-857bf85833b2",
+          src: "https://plausible.pandadev.net/js/script.js",
+          defer: true,
+          "data-domain": "essentialsp.tk",
         },
       ],
     },
